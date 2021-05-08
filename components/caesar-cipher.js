0a91f3c8ff699fd0869434cb7f93ab3a59f94f1f
Vlad Meleshko <vladik4.98@mail.ru>
2021-05-08 17:49:38 +0300

    feat: add caesar cipher function

--- components/caesar-cipher.js
+++ components/caesar-cipher.js
@@ -2,12 +2,27 @@ function caesarCipher(str, shift) {
     const arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     const arr_EN = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-    // str = str.split('');
+    const map = Array.prototype.map;
 
-    // str.forEach(strItem => {
-    //     strItem.map()
-    // })
-    // console.log(123)
+    if (shift > 26 || shift < -26) shift = shift % 26;
+    
+    if (shift < 0) shift = 26 + shift;
+
+    const newStr = map.call(str, strItem => {
+        let newLetter;
+        
+        const lowerCaseLetter = arr_en.indexOf(strItem);
+
+        const upperCaseLetterr = arr_EN.indexOf(strItem);
+
+        if (lowerCaseLetter !== -1) newLetter = arr_en[(lowerCaseLetter + shift) % 26];
+        if (upperCaseLetterr !== -1) newLetter = arr_EN[(upperCaseLetterr + shift) % 26];
+        if (lowerCaseLetter === -1 && upperCaseLetterr === -1) newLetter = strItem;
+
+        return newLetter;
+    }).join('');
+
+    return newStr;
 }
 
 module.exports = {caesarCipher};
