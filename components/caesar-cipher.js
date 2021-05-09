c96e715350c5bd75d8ecf4dcb88c9389e502c81e
Vlad Meleshko <vladik4.98@mail.ru>
2021-05-09 13:55:59 +0300

    feat: add work with streams

--- components/caesar-cipher.js
+++ components/caesar-cipher.js
@@ -1,4 +1,6 @@
-function caesarCipher(str, shift) {
+function caesarCipher(str, unsaveShift) {
+    let shift = Number(unsaveShift);
+    
     const arr_en = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     const arr_EN = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
--- components/main.js
+++ components/main.js
@@ -1,5 +1,29 @@
-const { createReadStreamCall } = require('./fileWorkServices');
+const fs = require('fs');
+const { stdinCommand } = require('./caesar-cipler-cli');
 const { caesarCipher } = require('./caesar-cipher');
+const { Transform } = require('stream');
+const { action, shift, input, output } = stdinCommand();
 
-createReadStreamCall('test.txt');
-caesarCipher();
+let readableStream;
+if (input) readableStream = fs.createReadStream(input);
+
+let writableStream;
+if (output) writableStream = fs.createWriteStream(output, {flags: 'a'});
+
+const caesarCipherTransform = new Transform({
+    transform(chunk, encoding, callback) {
+        if (action === 'encode') this.push(caesarCipher(chunk.toString(), shift));
+        if (action === 'decode') this.push(caesarCipher(chunk.toString(), -shift));
+        callback();
+    }
+});
+
+if (input && output) {
+    readableStream.pipe(caesarCipherTransform).pipe(writableStream);
+} else if (input && !output) {
+    readableStream.pipe(caesarCipherTransform).pipe(process.stdout);
+} else if (!input && output) {
+    process.stdin.pipe(caesarCipherTransform).pipe(writableStream);
+} else if (!input && !output) {
+    process.stdin.pipe(caesarCipherTransform).pipe(process.stdout);
+}
